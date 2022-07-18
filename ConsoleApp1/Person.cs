abdb0a1c7bed252004e802a9f5927838355f2899
istiaqhaider8 <istiaqhaider8@gmail.com>
2022-07-18 11:37:33 +0600

    Contructor Overloading

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public string firstName;
+        public string middleName;
+        public string lastName;
+
+
+
+        public Person(string firstName, string middleName, string lastName)
+           : this(lastName, firstName)
+        {
+
+            this.middleName = middleName;
+
+
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+
+        
+
+      
+
+        public string GetFullName()
+        {
+            return firstName + " " + middleName + " " + lastName;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person aPerson = new Person("Istiaq", "Hridon");
+            string fullName = aPerson.GetFullName();
+            Console.WriteLine(fullName);
+
+            Person bPerson = new Person("Istiaq", "Haider", "Hridon");
+            string fullName1 = bPerson.GetFullName();
+            Console.WriteLine(fullName1);
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
